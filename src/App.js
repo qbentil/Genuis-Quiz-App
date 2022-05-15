2e3d1a54b107af48d9d082fa7ac9127217e9198d
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-15 02:13:34 +0000

    Update ROutes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages";
+import HighScroes from "./pages/HighScores";
+import Game from "./pages/Game";
+import End from "./pages/End";
+import Select from "./components/Select";
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/highscores" element={<HighScroes />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/complete" element={<End />} />
+        <Route path="/select" element={<Select />} />
+      </Routes>
     </div>
   );
 }
