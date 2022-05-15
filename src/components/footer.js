b36bd2cfd10ecc603197a7162e6ad2a08c29948a
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-15 02:12:39 +0000

    Add Footer

---
+++ src/components/footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="footer">
+        <div id="footer-body" className="flex-center flex-column">
+          <p>WORLD QUIZ APP&copy; 2022</p>
+
+          <a href="http://qbentil.com" target="_blank" rel="noreferrer">
+            Made with ❤ by TEAM GENUIS
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
