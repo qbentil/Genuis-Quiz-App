5ec84e9e3e634173d38b08d20cc05baba411759c
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-16 11:04:45 +0000

    Create Selector Data

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const Categories = [
+    {
+        id: 'default',
+        title: 'Default'
+    },
+    {
+      id: 1,
+      title: "General",
+    },
+    {
+      id: 2,
+      title: "Programming",
+    },
+    {
+      id: 3,
+      title: "Mathematics",
+    },
+    {
+      id: 4,
+      title: "Sports",
+    },
+  ];
+
+export const Difficulties = [
+    {
+        id: 'default',
+        title: 'Default'
+    },
+    {
+        id: 'easy',
+        title: 'Easy'
+    },
+    {
+        id: 'medium',
+        title: 'Medium'
+    },
+    {
+        id: 'hard',
+        title: 'Hard'
+    },
+]
