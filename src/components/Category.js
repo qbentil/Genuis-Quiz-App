7226ba859f9fe2458e882f2f5711391524c6c342
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-18 22:13:32 +0000

    Render Selector on Catorgy page

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,12 +1,14 @@
 import { Categories } from "../data";
 import React from "react";
+import Select from "./Select";
 import Selector from "./Selector";
 
 const Category = ({setCategory}) => {
   return (
     <div id="end" className="flex-center flex-column">
       <h1>-- Select Category --</h1>
-        <Selector data={Categories} action = {setCategory} />
+        <Select options={Categories} action = {setCategory} />
+        {/* <Selector data={Categories} action = {setCategory} /> */}
 
     </div>
   );
