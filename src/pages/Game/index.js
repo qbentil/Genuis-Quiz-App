2268e9c076ac15e95481469c70709aba4bab750c
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-18 22:13:41 +0000

    Update Games

--- src/pages/Game/index.js
+++ src/pages/Game/index.js
@@ -1,57 +1,47 @@
-import './style.css'
+import "./style.css";
+
+import React, { useState } from "react";
 
 import Footer from "../../components/footer";
-import React from "react";
+import QuizItem from "../../components/QuizItem";
+import axios from "axios";
 
 const Game = () => {
+
+  const {category, difficulty} = JSON.parse(localStorage.getItem("game_info"));
+  const MAX_QUESTIONS = 10;
+  const [questionsCounter, setQuestionsCounter] = useState(0);
+  // const [currentQuestion, setCurrentQuestion] = useState();
+  const [questions, setQuestions] = useState([]);
+
+  
+  const generateQuestions = async (num, cat, dif) => {
+    const options = {
+      params: {
+        amount: num,
+        category: cat,
+        difficulty: dif,
+        type: 'multiple'
+      }
+  }
+  let url = `https://opentdb.com/api.php`
+  
+    try {
+        let res = await axios.get(url, options)
+        let data = await res.data.results
+        // console.log(data);
+        setQuestions(data)
+    } catch (e) {
+        console.log(e);
+    }
+  }
+  questions.length <= 0 && generateQuestions(MAX_QUESTIONS, category, difficulty)
   return (
     <div>
       <div className="container-alt">
-        <div id="loader hidden"></div>
-        <div id="game" className="text-center flex-column">
-          <div id="hud">
-            <div id="hud-item">
-              <p className="hud-prefix">
-                Question: <span id="questionCounter"></span>
-              </p>
-              <div className="progress-bar">
-                <div className="progress-bar-full" id="progress-bar"></div>
-              </div>
-            </div>
-
-            <div id="hud-item">
-              <p className="hud-prefix">Score</p>
-              <h1 className="hud-main-text text-center" id="score">
-                0
-              </h1>
-            </div>
-          </div>
-          <h2 id="question">What is the answer to this question?</h2>
-          <div className="choice-container">
-            <p className="choice-prefix">A</p>
-            <p className="choice-text" data-number="1">
-              Choice 1
-            </p>
-          </div>
-          <div className="choice-container">
-            <p className="choice-prefix">B</p>
-            <p className="choice-text" data-number="2">
-            Infinitbility is a tech blog created with the sole purpose of explaining complex tech in a simple and concise way thus creating value for budding developers out there.
-            </p>
-          </div>
-          <div className="choice-container">
-            <p className="choice-prefix">C</p>
-            <p className="choice-text" data-number="3">
-              Choice 3
-            </p>
-          </div>
-          <div className="choice-container">
-            <p className="choice-prefix">D</p>
-            <p className="choice-text" data-number="4">
-              Choice 4
-            </p>
-          </div>
-        </div>
+        {
+          questions.length <= 0 ?( <div id="loader"></div>):(<QuizItem max = {MAX_QUESTIONS} count = {questionsCounter} action = {setQuestionsCounter} question = {questions[questionsCounter]} />)
+        }
       </div>
       <Footer />
     </div>
